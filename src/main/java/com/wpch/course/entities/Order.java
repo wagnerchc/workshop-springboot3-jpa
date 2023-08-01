2207b3e80d547fb9f28178f9d28d7fa34ec9fdc6
wagnerchc <w.chequeleiro@gmail.com>
2023-08-01 15:51:14 -0300

    Subtotal & Total methods

--- src/main/java/com/wpch/course/entities/Order.java
+++ src/main/java/com/wpch/course/entities/Order.java
@@ -101,6 +101,14 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem obj : items) {
+			sum += obj.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
