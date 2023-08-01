94c48aec40f276b2f57937f93bd7572e53356c1f
wagnerchc <w.chequeleiro@gmail.com>
2023-08-01 11:09:56 -0300

    Order, Instant, ISO 8601 part 1

--- src/main/java/com/wpch/course/entities/User.java
+++ src/main/java/com/wpch/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.wpch.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
 	}
 
@@ -89,6 +95,10 @@ public class User implements Serializable {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 }
