26ed90f4f6531733719478cf5cfa3cb556b5e138
wagnerchc <w.chequeleiro@gmail.com>
2023-08-01 14:19:51 -0300

    Category and product association

--- src/main/java/com/wpch/course/entities/Category.java
+++ src/main/java/com/wpch/course/entities/Category.java
@@ -9,8 +9,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -23,7 +23,7 @@ public class Category implements Serializable {
 	private String name;
 	
 	//Associations
-	@Transient //impede a interpretação do jpa, enquanto não está conclusa a associação
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
