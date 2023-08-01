873ac3067106f39c7ef12b4ec16478a91e0c2bc0
wagnerchc <w.chequeleiro@gmail.com>
2023-08-01 15:35:16 -0300

    Payment, one-to-one association

--- src/main/java/com/wpch/course/config/TestConfig.java
+++ src/main/java/com/wpch/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.wpch.course.entities.Category;
 import com.wpch.course.entities.Order;
 import com.wpch.course.entities.OrderItem;
+import com.wpch.course.entities.Payment;
 import com.wpch.course.entities.Product;
 import com.wpch.course.entities.User;
 import com.wpch.course.entities.enums.OrderStatus;
@@ -80,6 +81,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
