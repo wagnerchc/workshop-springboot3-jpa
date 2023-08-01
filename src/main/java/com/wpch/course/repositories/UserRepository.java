afb86f6faa628b98cfcc650d041f7c5710de60d7
wagnerchc <w.chequeleiro@gmail.com>
2023-08-01 09:45:44 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/wpch/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.wpch.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wpch.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+	
+}
